bfbb0d97971951869b5ff5c930149732d6110e04
Mateusz Widerski <mat.widerski@gmail.com>
2018-04-16 23:29:06 +0200

    Code refactoring

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 // var element = React.createElement('div', {}, 'Hello world!');
 // ReactDOM.render(element, document.getElementById('app'));
 
+var movies = [
+	{
+		title: 'Scarface',
+		desc: 'Movie tells the story of Cuban emigrant, Tony Montana'
+	},
+	{
+		title: 'Inception',
+		desc: 'Movie about thief, who steals corporate secrets through the use of dream-sharing technology'
+	}
+];
+
+var moviesElements = movies.map(function(movie) {
+	return React.createElement('li', {},
+		React.createElement('h2', {}, movie.title),
+		React.createElement('p', {}, movie.desc)
+	);
+});
+
 var element = 
 	React.createElement('div', {},
 		React.createElement('h1', {}, 'List of movies'),
-		React.createElement('ul', {},
-			React.createElement('li', {},
-				React.createElement('h2', {}, 'Scarface'),
-				React.createElement('p', {}, 'Movie about the Cuban emigrant, Tony Montana')
-			),
-			React.createElement('li', {},
-				React.createElement('h2', {}, 'Inception'),
-				React.createElement('p', {}, 'Movie about thief, who steals corporate secrets through the use of dream-sharing technology')
-			)
-		)
+		React.createElement('ul', {}, moviesElements)
 	);
 
 ReactDOM.render(element, document.getElementById('app'));
