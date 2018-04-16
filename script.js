ca2819176b1a4594dd499b5228fc46ed948f32e5
Mateusz Widerski <mat.widerski@gmail.com>
2018-04-16 23:19:19 +0200

    Add script for display list of movies

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-var element = React.createElement('div', {}, 'Hello world!');
+// var element = React.createElement('div', {}, 'Hello world!');
+// ReactDOM.render(element, document.getElementById('app'));
+
+var element = 
+	React.createElement('div', {},
+		React.createElement('h1', {}, 'List of movies'),
+		React.createElement('ul', {},
+			React.createElement('li', {},
+				React.createElement('h2', {}, 'Scarface'),
+				React.createElement('p', {}, 'Movie about the Cuban emigrant, Tony Montana')
+			),
+			React.createElement('li', {},
+				React.createElement('h2', {}, 'Inception'),
+				React.createElement('p', {}, 'Movie about thief, who steals corporate secrets through the use of dream-sharing technology')
+			)
+		)
+	);
+
 ReactDOM.render(element, document.getElementById('app'));
